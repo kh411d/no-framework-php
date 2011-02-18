011f6b983f1088d7b5f8872c3ae233d2a2289e04
Khalid Adisendjaja <kh411d@yahoo.com>
2011-02-18 14:14:42 +0700

    fix

--- library/session.php
+++ library/session.php
@@ -8,14 +8,12 @@
 	
 	public function set($key,$value = NULL)
 	{
-	 if(empty($key))die('Session Key undefined');
 	 $_SESSION[$key] = $value;
 	 return $value;
 	}
 	
 	public function get($key)
 	{
-	 if(empty($key))die('Session Key undefined');
 	 return $_SESSION[$key];
 	}
 	
